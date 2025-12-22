4a0ca1d2076cbcb7b84f4b5f4e2dc4d33cb725dc
Emanuel <emanuelwellygton@gmail.com>
2025-12-22 20:48:14 -0300

    feat: initialize Spring Boot project with Maven Wrapper

---
+++ src/main/java/dev/wellyngton/doable/DoableApplication.java
@@ -0,0 +1,13 @@
+package dev.wellyngton.doable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/wellyngton/doable/DoableApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wellyngton.doable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
